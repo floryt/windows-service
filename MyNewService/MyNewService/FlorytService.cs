a8f57755b59f831d30742feaa7946c6da8cfb692
BarAdar4 <baradar4@gmail.com>
2017-06-14 01:06:36 +0300

    deleted unncessary code

--- MyNewService/MyNewService/FlorytService.cs
+++ MyNewService/MyNewService/FlorytService.cs
@@ -158,7 +158,7 @@ namespace FlorytService
             request.Method = "POST";
             // Create POST data and convert it to a byte array.
             string status = GetStatus();
-            string postData = "{\"computerUid\":\"" + strUID + "\", \"status\":\"" + status + "\", \"i\":\"0\"}";
+            string postData = "{\"computerUid\":\"" + strUID + "\", \"status\":\"" + status + "\", \"i\":\"1\"}";
             //Console.WriteLine(postData);
             byte[] byteArray = Encoding.UTF8.GetBytes(postData);
             // Set the ContentType property of the WebRequest.
@@ -215,7 +215,7 @@ namespace FlorytService
                         {
                             eventLog2.WriteEntry("present message", EventLogEntryType.Information);
                             //LockWorkStation();
-                            WriteToFile("present_message" + "hello");//array.message);
+                            WriteToFile("present_message" + "hello");//array.message); in comment because Steven forgot to add it to the mock. -.-
                         }
                         break;
                     default:
@@ -274,81 +274,6 @@ namespace FlorytService
         }
 
 
-
-        [DllImport("wtsapi32.dll", SetLastError = true)]
-        static extern bool WTSDisconnectSession(IntPtr hServer, int sessionId, bool bWait);
-
-        [DllImport("wtsapi32.dll", SetLastError = true)]
-        static extern int WTSEnumerateSessions(IntPtr hServer, int Reserved, int Version, ref IntPtr ppSessionInfo, ref int pCount);
-
-        [DllImport("wtsapi32.dll")]
-        static extern void WTSFreeMemory(IntPtr pMemory);
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct WTS_SESSION_INFO
-        {
-            public Int32 SessionID;
-
-            [MarshalAs(UnmanagedType.LPStr)]
-            public String pWinStationName;
-
-            public WTS_CONNECTSTATE_CLASS State;
-        }
-
-        private enum WTS_INFO_CLASS
-        {
-            WTSInitialProgram,
-            WTSApplicationName,
-            WTSWorkingDirectory,
-            WTSOEMId,
-            WTSSessionId,
-            WTSUserName,
-            WTSWinStationName,
-            WTSDomainName,
-            WTSConnectState,
-            WTSClientBuildNumber,
-            WTSClientName,
-            WTSClientDirectory,
-            WTSClientProductId,
-            WTSClientHardwareId,
-            WTSClientAddress,
-            WTSClientDisplay,
-            WTSClientProtocolType
-        }
-
-        private enum WTS_CONNECTSTATE_CLASS
-        {
-            WTSActive,
-            WTSConnected,
-            WTSConnectQuery,
-            WTSShadow,
-            WTSDisconnected,
-            WTSIdle,
-            WTSListen,
-            WTSReset,
-            WTSDown,
-            WTSInit
-        }
-
-        public static void LockWorkStation()
-        {
-            IntPtr ppSessionInfo = IntPtr.Zero;
-            Int32 count = 0;
-            Int32 retval = WTSEnumerateSessions(IntPtr.Zero, 0, 1, ref ppSessionInfo, ref count);
-            Int32 dataSize = Marshal.SizeOf(typeof(WTS_SESSION_INFO));
-            Int32 currentSession = (int)ppSessionInfo;
-
-            if (retval == 0) return;
-
-            for (int i = 0; i < count; i++)
-            {
-                WTS_SESSION_INFO si = (WTS_SESSION_INFO)Marshal.PtrToStructure((System.IntPtr)currentSession, typeof(WTS_SESSION_INFO));
-                if (si.State == WTS_CONNECTSTATE_CLASS.WTSActive) WTSDisconnectSession(IntPtr.Zero, si.SessionID, false);
-                currentSession += dataSize;
-            }
-            WTSFreeMemory(ppSessionInfo);
-        }
-
         protected override void OnShutdown()
         {
             string strUID = getUID();
@@ -379,40 +304,6 @@ namespace FlorytService
             //WebResponse response = request.GetResponse();
         }
 
-        [StructLayout(LayoutKind.Sequential, Pack = 1)]
-        internal struct TokPriv1Luid
-        {
-            public int Count;
-            public long Luid;
-            public int Attr;
-        }
-
-        [DllImport("kernel32.dll", ExactSpelling = true)]
-        internal static extern IntPtr GetCurrentProcess();
-
-        [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
-        internal static extern bool OpenProcessToken(IntPtr h, int acc, ref IntPtr phtok);
-
-        [DllImport("advapi32.dll", SetLastError = true)]
-        internal static extern bool LookupPrivilegeValue(string host, string name, ref long pluid);
-
-        [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
-        internal static extern bool AdjustTokenPrivileges(IntPtr htok, bool disall,
-            ref TokPriv1Luid newst, int len, IntPtr prev, IntPtr relen);
-
-        [DllImport("user32.dll", ExactSpelling = true, SetLastError = true)]
-        internal static extern bool ExitWindowsEx(int flg, int rea);
-
-        internal const int SE_PRIVILEGE_ENABLED = 0x00000002;
-        internal const int TOKEN_QUERY = 0x00000008;
-        internal const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
-        internal const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
-        internal const int EWX_LOGOFF = 0x00000000;
-        internal const int EWX_SHUTDOWN = 0x00000001;
-        internal const int EWX_REBOOT = 0x00000002;
-        internal const int EWX_FORCE = 0x00000004;
-        internal const int EWX_POWEROFF = 0x00000008;
-        internal const int EWX_FORCEIFHUNG = 0x00000010;
 
         private void WriteToFile(string command)
         {
