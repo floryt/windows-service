9fb08593aeb22dd203db76f2f2cd97fcdcce94f9
Steven Dashevsky <stiven1999d@gmail.com>
2017-07-08 00:33:36 +0300

    Added interactive process implementation.

---
+++ Executioner/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Executioner
+{
+    public partial class Message : Form
+    {
+        public Message(string message)
+        {
+            InitializeComponent();
+        }
+    }
+}
